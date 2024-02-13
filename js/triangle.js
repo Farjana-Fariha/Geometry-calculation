f9a8fc4561245b3e25f4d71b5f5ccb64cf36b13c
Farjana-Fariha <farjanafariha507@gmail.com>
2024-02-13 16:59:45 +0600

    view part completed

---
+++ js/triangle.js
@@ -0,0 +1,11 @@
+console.log('connected');
+function calculateTriangle(){
+   // get width
+   const triangleWidth = document.getElementById('triangle-width').value;
+   const width = parseFloat(triangleWidth);
+   // Get height
+   const triangleHeight = document.getElementById('triangle-height').value;
+   const height = parseFloat(triangleHeight);
+
+   console.log(height);
+}
